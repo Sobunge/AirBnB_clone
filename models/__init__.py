f59301e20333b5c8d6026225187c04f90009bdc9
samuel obunge <samuelobunge@gmail.com>
2024-02-07 13:57:51 -0800

    adding task 3 Base Model

---
+++ models/__init__.py
@@ -0,0 +1,2 @@
+#!/usr/bin/python3
+from .base_model import BaseModel
---
+++ models/base_model.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        
+        # Rearrange the keys in the dictionary
+        ordered_keys = ['my_number', 'name', '__class__', 'updated_at', 'id', 'created_at']
+        obj_dict_ordered = {key: obj_dict[key] for key in ordered_keys if key in obj_dict}
+
+        return obj_dict_ordered
