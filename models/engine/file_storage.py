7d9898f7286b6e1607deae86a7b8affd93e7d4f5
ogemboeugene <7103.2021@students.ku.ac.ke>
2024-02-11 12:07:24 -0800

    sixth change

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,9 +5,17 @@ Contains the FileStorage class
 
 import json
 import models
+from models.amenity import Amenity
 from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from hashlib import md5
 
-classes = {"BaseModel": BaseModel}
+classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": User}
 
 
 class FileStorage:
@@ -51,12 +59,8 @@ class FileStorage:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except FileNotFoundError:
-            print("Error: File not found.")
-        except json.JSONDecodeError:
-            print("Error: JSON decoding failed.")
-        except Exception as e:
-            print(f"An unexpected error occurred: {str(e)}")
+        except:
+            pass
 
     def delete(self, obj=None):
         """delete obj from __objects if it’s inside"""
