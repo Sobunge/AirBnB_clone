941d971e761bc229037e6cca039cd9d0eb7598fd
ogemboeugene <7103.2021@students.ku.ac.ke>
2024-02-11 12:02:59 -0800

    fifth change

--- console.py
+++ console.py
@@ -1,184 +1,164 @@
 #!/usr/bin/python3
-""" Adding console module documentation """
+""" console """
+
 import cmd
-import json
+from datetime import datetime
+import models
+from models.amenity import Amenity
 from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+import shlex  # for splitting the line along spaces except in double quotes
+
+classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": User}
 
 
 class HBNBCommand(cmd.Cmd):
-    """ Adding HBNBCommane documentation """
-    prompt = "(hbnb) "
+    """ HBNH console """
+    prompt = '(hbnb) '
+
+    def do_EOF(self, arg):
+        """Exits console"""
+        return True
+
+    def emptyline(self):
+        """ overwriting the emptyline method """
+        return False
 
     def do_quit(self, arg):
         """Quit command to exit the program"""
         return True
 
-    def do_EOF(self, arg):
-        """Handles EOF (Ctrl+D)"""
-        print("^D")
-        return True
-
-    def emptyline(self):
-        """Do nothing when an empty line is entered"""
-        pass
+    def _key_value_parser(self, args):
+        """creates a dictionary from a list of strings"""
+        new_dict = {}
+        for arg in args:
+            if "=" in arg:
+                kvp = arg.split('=', 1)
+                key = kvp[0]
+                value = kvp[1]
+                if value[0] == value[-1] == '"':
+                    value = shlex.split(value)[0].replace('_', ' ')
+                else:
+                    try:
+                        value = int(value)
+                    except:
+                        try:
+                            value = float(value)
+                        except:
+                            continue
+                new_dict[key] = value
+        return new_dict
 
     def do_create(self, arg):
-        """Creates a new instance of BaseModel"""
-        if not arg:
+        """Creates a new instance of a class"""
+        args = arg.split()
+        if len(args) == 0:
             print("** class name missing **")
-            return
-
-        try:
-            new_instance = eval(arg)()
-            new_instance.save()
-            print(new_instance.id)
-        except NameError:
+            return False
+        if args[0] in classes:
+            new_dict = self._key_value_parser(args[1:])
+            instance = classes[args[0]](**new_dict)
+        else:
             print("** class doesn't exist **")
+            return False
+        print(instance.id)
+        instance.save()
 
     def do_show(self, arg):
-        """Prints the string representation of an instance"""
-        if not arg:
+        """Prints an instance as a string based on the class and id"""
+        args = shlex.split(arg)
+        if len(args) == 0:
             print("** class name missing **")
-            return
-
-        args = arg.split()
-        if len(args) < 1:
-            print("** class name missing **")
-            return
-
-        if len(args) < 2:
-            print("** instance id missing **")
-            return
-
-        try:
-            with open("file.json", "r") as file:
-                objects = json.load(file)
-        except FileNotFoundError:
-            print("** no instance found **")
-            return
-
-        key = args[0] + "." + args[1]
-        if key not in objects:
-            print("** no instance found **")
-            return
-
-        print(objects[key])
+            return False
+        if args[0] in classes:
+            if len(args) > 1:
+                key = args[0] + "." + args[1]
+                if key in models.storage.all():
+                    print(models.storage.all()[key])
+                else:
+                    print("** no instance found **")
+            else:
+                print("** instance id missing **")
+        else:
+            print("** class doesn't exist **")
 
     def do_destroy(self, arg):
-        """Deletes an instance based on the class name and id"""
-        if not arg:
+        """Deletes an instance based on the class and id"""
+        args = shlex.split(arg)
+        if len(args) == 0:
             print("** class name missing **")
-            return
-
-        args = arg.split()
-        if len(args) < 1:
-            print("** class name missing **")
-            return
-
-        if len(args) < 2:
-            print("** instance id missing **")
-            return
-
-        try:
-            with open("file.json", "r") as file:
-                objects = json.load(file)
-        except FileNotFoundError:
-            print("** no instance found **")
-            return
-
-        key = args[0] + "." + args[1]
-        if key not in objects:
-            print("** no instance found **")
-            return
-
-        # Delete the instance
-        del objects[key]
-
-        # Save the changes back to the JSON file
-        with open("file.json", "w") as file:
-            json.dump(objects, file)
+        elif args[0] in classes:
+            if len(args) > 1:
+                key = args[0] + "." + args[1]
+                if key in models.storage.all():
+                    models.storage.all().pop(key)
+                    models.storage.save()
+                else:
+                    print("** no instance found **")
+            else:
+                print("** instance id missing **")
+        else:
+            print("** class doesn't exist **")
 
     def do_all(self, arg):
-        """Prints all string representation of all instances"""
-        try:
-            with open("file.json", "r") as file:
-                objects = json.load(file)
-        except FileNotFoundError:
-            print("** class doesn't exist **")
-            return
-
-        if arg:
-            class_name = arg.split()[0]
-            instances = [v for k, v in objects.items()
-                         if k.startswith(class_name + ".")]
+        """Prints string representations of instances"""
+        args = shlex.split(arg)
+        obj_list = []
+        if len(args) == 0:
+            obj_dict = models.storage.all()
+        elif args[0] in classes:
+            obj_dict = models.storage.all(classes[args[0]])
         else:
-            instances = list(objects.values())
-
-        if not instances:
             print("** class doesn't exist **")
-        else:
-            for instance in instances:
-                print(instance)
+            return False
+        for key in obj_dict:
+            obj_list.append(str(obj_dict[key]))
+        print("[", end="")
+        print(", ".join(obj_list), end="")
+        print("]")
 
     def do_update(self, arg):
-        """Updates an instance based on the class name and id"""
-        if not arg:
+        """Update an instance based on the class name, id, attribute & value"""
+        args = shlex.split(arg)
+        integers = ["number_rooms", "number_bathrooms", "max_guest",
+                    "price_by_night"]
+        floats = ["latitude", "longitude"]
+        if len(args) == 0:
             print("** class name missing **")
-            return
+        elif args[0] in classes:
+            if len(args) > 1:
+                k = args[0] + "." + args[1]
+                if k in models.storage.all():
+                    if len(args) > 2:
+                        if len(args) > 3:
+                            if args[0] == "Place":
+                                if args[2] in integers:
+                                    try:
+                                        args[3] = int(args[3])
+                                    except:
+                                        args[3] = 0
+                                elif args[2] in floats:
+                                    try:
+                                        args[3] = float(args[3])
+                                    except:
+                                        args[3] = 0.0
+                            setattr(models.storage.all()[k], args[2], args[3])
+                            models.storage.all()[k].save()
+                        else:
+                            print("** value missing **")
+                    else:
+                        print("** attribute name missing **")
+                else:
+                    print("** no instance found **")
+            else:
+                print("** instance id missing **")
+        else:
+            print("** class doesn't exist **")
 
-        args = arg.split()
-        if len(args) < 1:
-            print("** class name missing **")
-            return
-
-        if len(args) < 2:
-            print("** instance id missing **")
-            return
-
-        try:
-            with open("file.json", "r") as file:
-                objects = json.load(file)
-        except FileNotFoundError:
-            print("** no instance found **")
-            return
-
-        key = args[0] + "." + args[1]
-        if key not in objects:
-            print("** no instance found **")
-            return
-
-        if len(args) < 3:
-            print("** attribute name missing **")
-            return
-
-        if len(args) < 4:
-            print("** value missing **")
-            return
-
-        attribute_name = args[2]
-        attribute_value = args[3]
-
-        # Update the attribute value
-        objects[key][attribute_name] = attribute_value
-
-        # Save the changes back to the JSON file
-        with open("file.json", "w") as file:
-            json.dump(objects, file)
-
-    def do_count(self, arg):
-        """Counts the number of instances of a class"""
-        if not arg:
-            print("** class name missing **")
-            return
-
-        count = 0
-        objects = BaseModel().all()
-        for key in objects:
-            if key.split(".")[0] == arg:
-                count += 1
-        print(count)
-
-
-if __name__ == "__main__":
-    prompt = HBNBCommand()
-    prompt.cmdloop()
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
