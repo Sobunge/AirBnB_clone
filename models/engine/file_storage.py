bc5d98f5dd542e48844858e3a33478e3c35269f0
samuel obunge <samuelobunge@gmail.com>
2024-02-11 12:28:20 -0800

    pycodestyle fix

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -59,8 +59,12 @@ class FileStorage:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except:
-            pass
+        except FileNotFoundError:
+            # Handle the case where the JSON file is not found
+            print(f"Error: JSON file '{self.__file_path}' not found")
+        except Exception as e:
+            # Handle any other exceptions
+            print(f"Error: An unexpected error occurred: {e}")
 
     def delete(self, obj=None):
         """delete obj from __objects if it’s inside"""
